b3b96bb2fe31f8e2cc01698d3d4df382fb2644bb
Pragyan Budhathoki <132291908+pragyanbt@users.noreply.github.com>
2025-02-18 14:54:48 -0500

    Update Header.tsx

--- NutriByte/src/components/Header.tsx
+++ NutriByte/src/components/Header.tsx
@@ -3,9 +3,17 @@ import { fadeIn } from "../utils/variants";
 
 const Header = () => {
   return (
-    <motion.nav variants={fadeIn("down")} initial="initial" animate="animate" className="header">
+    <motion.nav variants={fadeIn("down")} initial="initial" animate="animate" className="header flex justify-between items-center">
       <span className="header-logo">NutriByte</span>
-      <ul>
+
+      {/* Hamburger Icon for minimized navbar */}
+      <div className="md:hidden flex flex-col justify-around items-center w-6 h-6 cursor-pointer">
+        <div className="w-full h-1 bg-white"></div>
+        <div className="w-full h-1 bg-white"></div>
+        <div className="w-full h-1 bg-white"></div>
+      </div>
+
+      <ul className="hidden md:flex">
         <li>HOME</li>
         <li>PLANNER</li>
         <li>TRACK</li>
@@ -14,7 +22,7 @@ const Header = () => {
       </ul>
 
       <span className="header-account">My Account</span>
-      <span className="header-user">🤕</span>
+      <span className="header-user">A</span>
     </motion.nav>
   );
 };
